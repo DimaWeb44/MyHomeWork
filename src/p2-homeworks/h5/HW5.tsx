eb94b1670358c8e19898f2ba37c1f35cd7b8bd35
DimaWeb44 <dima.web44@gmail.com>
2022-02-01 20:21:18 +0300

    HW5

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
+import {HashRouter} from 'react-router-dom'
 import Header from './Header'
-import Routes from './Routes'
+import Routes1 from './Routes'
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
-
-            <Header/>
-
-            <Routes/>
-
-            {/*</HashRouter>*/}
+            <HashRouter>
+                <Header/>
+                <Routes1/>
+            </HashRouter>
         </div>
     )
 }
--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { PATH } from './Routes'
+import {NavLink} from "react-router-dom";
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.hamburger_menu}>
+            <input id={s.menu__toggle} type="checkbox"/>
+            <label className={s.menu__btn} htmlFor={s.menu__toggle}>
+                <span></span>
+            </label>
+            <div className={s.menu__box}>
+                <NavLink to={PATH.PRE_JUNIOR} className={s.menu__item}>  PreJunior</NavLink>
+                <NavLink to={PATH.JUNIOR} className={s.menu__item}>  Junior</NavLink>
+                <NavLink to={PATH.JUNIOR_PLUS} className={s.menu__item}>  JuniorPlus</NavLink>
+            </div>
         </div>
     )
 }
